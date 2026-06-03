57262df557609ec301e5bb812d436e65045e12cb
Harsha <harshavardhanah03@gmail.com>
2026-06-03 18:55:17 +0530

    Introduced Enums for handling the codes for the Create Job API test.
    Reduced Magic Number Issue in the test script.

--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -9,7 +9,14 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warranty_Status;
 import com.api.request.model.CreateJobPayload;
 import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
@@ -32,12 +39,14 @@ public class CreateJobAPITest {
 		
 		Customer customer = new Customer("Harsha", "vardhana", "9980070086", "", "harsha.vardhan@gmail.com", "");
 		CustomerAddress customerAddress = new CustomerAddress("D 185", "SLV Nilaya", "Vaddarahalli", "Kylancha", "Ramanagara", "562159", "India", "Karnataka");
-		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "18849695824646", "18849695824646", "18849695824646", getTimeWithDaysAgo(10), 1, 1);
-		Problems problems = new Problems(1, "Battery Issue"); 
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "19849695824646", "19849695824646", "19849695824646", getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
+		Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue"); 
+		
+		
 		List<Problems> problemList = new ArrayList<Problems>();
 		problemList.add(problems);
 		
-		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemList);
+		CreateJobPayload createJobPayload = new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warranty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(), customer, customerAddress, customerProduct, problemList);
 		
 		given()
 		.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
